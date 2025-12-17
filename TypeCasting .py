97c302bff6ce9d27f1e742e3a43a661feb2b16d1
suraj7547 <139971831+suraj7547@users.noreply.github.com>
2025-12-17 19:31:11 +0530

    Update and rename TyepCasinting.py to TypeCasting .py

---
+++ TypeCasting .py
@@ -0,0 +1,16 @@
+# Typecasting = the process of converting a variable form one data type to another
+# str(),int(),float(),bool()
+name = "suraj"
+age = 20
+gpa =23.34
+student = True
+
+age = str(age)
+print(age)
+print(type(name))
+
+print("------------------------")
+
+name = bool(name)
+print (name) #if there is nothing in the name vairalbe then it will give the result as false
+print (type(name))
