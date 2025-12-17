a910b63fa26894aee838a23fd26ab87aa2589b71
suraj7547 <139971831+suraj7547@users.noreply.github.com>
2025-12-17 19:31:57 +0530

    Create Area Of Rectangle.py

---
+++ Area Of Rectangle.py
@@ -0,0 +1,8 @@
+#Area of Reactanlge with imput
+
+length = int(input("Enter the length: "))
+breadth = int(input("Enter the breadth: "))
+Area = length * breadth
+Perimeter = 2*(length + breadth)
+print(f"Area: {Area}cm²")
+print(f"Perimeter: {Perimeter}cm")
