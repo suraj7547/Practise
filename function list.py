ce6ebf82efb4e8888a9e70f4d76d7a3630554654
suraj7547 <139971831+suraj7547@users.noreply.github.com>
2026-01-03 23:54:46 +0530

    Create function list.py

---
+++ function list.py
@@ -0,0 +1,15 @@
+city = ["delhi", "bihar","pune", "goa"]
+objects = ["pen","pencil","phone","eraiser"]
+
+
+def length_string(city):
+    print(len(city))
+
+def length_string(objects):
+    print(len(objects))
+
+length_string(city)
+length_string(objects)
+
+for item in city:
+    print(item,end = " ")
