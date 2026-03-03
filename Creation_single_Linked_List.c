b2a5dbcbaca9ecd678039799fcec6631e5852a87
suraj7547 <139971831+suraj7547@users.noreply.github.com>
2026-03-03 18:36:23 +0530

    Create Creation_single_Linked_List.c

---
+++ Creation_single_Linked_List.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *link;
+}*head = NULL;
+
+void nodecount(struct node *head){
+    int count=0;
+    if(head==NULL) printf("Linked list is empty\n");
+    struct node *ptr=head;
+    while(ptr!=NULL){
+        count++;
+        ptr=ptr->link;
+    }
+    printf("%d\n",count);
+}
+void printnode(struct node *head){
+    if(head==NULL) printf("Linked list is empty\n");
+    struct node *ptr=head;
+    while(ptr!=NULL){
+        printf("%d -> ",ptr->data);
+        ptr=ptr->link;
+    }
+    printf(" NULL");
+}
+int main(){
+    struct node *head=(struct node*)malloc(sizeof(struct node));
+    head->data=34;
+    head->link=NULL;
+    
+    struct node *current=(struct node*)malloc(sizeof(struct node));
+    current->data=32;
+    current->link=NULL;
+    head->link=current;
+
+    struct node *current1=(struct node*)malloc(sizeof(struct node));
+    current1->data=52;
+    current1->link=NULL;
+    head->link->link=current1;
+
+    printf("%d\n",head->link->data);
+    nodecount(head);
+    printnode(head);
+    return 0;
+    
+
+}
