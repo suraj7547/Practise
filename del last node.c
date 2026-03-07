79be3ddddb7c8f33cd3f7c71c484267be88fc2e5
suraj7547 <139971831+suraj7547@users.noreply.github.com>
2026-03-07 14:46:54 +0530

    Create del last node.c

---
+++ del last node.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+}*head=NULL;
+
+
+void print(struct node *head){
+    struct node *ptr=head;
+    while(ptr!=NULL){
+        printf("%d -> ",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("NULL\n");
+}
+
+void nodeatend(struct node* head,int data){
+    struct node *ptr=head;
+    struct node *temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->next=NULL;
+    while(ptr->next!=NULL){
+        ptr=ptr->next;
+    }
+    ptr->next=temp;
+}
+
+struct node *del_last_node(struct node *head){
+    if(head==NULL){
+        printf("Linked list is empty");
+    }
+    else if (head->next==NULL)
+    {
+        free(head);
+        head=NULL;
+    }
+    else{
+        struct node *last=head;
+        struct node *secondlast=head;
+        while(last->next!=NULL){
+            secondlast=last;
+            last=last->next;
+        }
+        secondlast->next=NULL;
+        free(last);
+        last=NULL;
+    }
+    return head;
+}
+
+int main(){
+    struct node *head=(struct node*)malloc(sizeof(struct node));
+    head->data=34;
+    head->next=NULL;
+    nodeatend(head,35);
+    nodeatend(head,36);
+    nodeatend(head,37);
+    head=del_last_node(head);
+    print(head);
+    return 0;
+}
