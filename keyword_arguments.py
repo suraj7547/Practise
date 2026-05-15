cc26ddc11f27b804d576c12b91e24431c8833297
SURAJ KUMAR <139971831+suraj7547@users.noreply.github.com>
2026-05-15 02:17:06 +0000

    function arguments

---
+++ keyword_arguments.py
@@ -0,0 +1,7 @@
+print("1","2","3","4",sep="@")
+
+def phone_number(country_code,phone_num):
+    return f"{country_code}{phone_num}"
+
+phone=phone_number(country_code="+91",phone_num="7547212345")
+print(phone)
