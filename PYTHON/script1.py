e09dd534d85f84ed58f3bebc27811e1382a6db93
SURAJ KUMAR <139971831+suraj7547@users.noreply.github.com>
2026-05-18 15:02:53 +0000

    written some functions

--- PYTHON/script1.py
+++ PYTHON/script1.py
@@ -1,3 +1,15 @@
 from script2 import *
 
-print(__name__)
+def favorite_food(food):
+    print(f"Your favorite food is :{food}")
+
+def favorite_drink(drink):
+    print(f"Your favorite drink is :{drink}")
+
+def main():
+    favorite_food("pizza")
+    favorite_drink("coffee")
+
+if __name__=="__main__":
+    main()
+
--- PYTHON/script2.py
+++ PYTHON/script2.py
@@ -1,4 +1,10 @@
+from script1 import *
+def favorite_book(book):
+    print(f"Your favorite book is :{book}")
+def main():
+    favorite_drink("coke")
+    favorite_food("mango")
+    favorite_book("rich dad poor dad")
 
-
-print(__name__)
-print("This is script two")
+if __name__ == "__main__":
+    main()
