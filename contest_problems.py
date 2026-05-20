c04bfe4834dcb97fda624e3eb9e6b83eb63eac88
SURAJ KUMAR <139971831+suraj7547@users.noreply.github.com>
2026-05-20 14:43:45 +0000

    second question

--- contest_problems.py
+++ contest_problems.py
@@ -1,4 +1,5 @@
-#first question
+#first question - Tour plan
+
 
 x, y, z = map(int, input().split())
 
@@ -6,3 +7,29 @@ if z <= 50:
     print(x)
 else:
     print(x + (z - 50) * y)
+
+#second question - easy speaking
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+    
+    consonant_count = 0
+    is_hard = False
+    
+    for char in s:
+        if char in 'aeiou':
+            consonant_count = 0
+        else:
+            consonant_count += 1
+            if consonant_count >= 4:
+                is_hard = True
+                break
+                
+    if is_hard:
+        print("Yes")
+    else:
+        print("No")
+        
