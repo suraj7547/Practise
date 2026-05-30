8a995c81d93e8774becd09cde88c73b5bf23f74c
SURAJ KUMAR <139971831+suraj7547@users.noreply.github.com>
2026-05-30 17:12:23 +0000

    Made a class student input system

--- input.java
+++ input.java
@@ -1,13 +1,33 @@
 import java.util.Scanner;
 
-public class input {
+public class input{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter your name : ");
-        String num=sc.nextLine();
-        System.out.println("Hello! "+num);
+        System.out.print("Enter your name: ");
+        String name=sc.nextLine();
+        System.out.print("Enter your age :");
+        int age=sc.nextInt();
+        System.out.print("Enter your grade: ");
+        double grade=sc.nextDouble();
+        System.out.print("Are you a student (true/false)?: ");
+        boolean isStudent=sc.nextBoolean();
         sc.close();
 
+        //printing the things
+
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-");
+
+        System.out.println("Hello! "+name);
+        System.out.println("Your age is "+age);
+        System.out.println("Your grade is "+grade);
+        if(isStudent){
+            System.out.println("You are enrolled in the classes");
+        }
+        else 
+        {
+            System.out.println("You are not enrolled in the classes");
+        }
+        
     }
-    
 }
