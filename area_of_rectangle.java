7490566cdf75db4809926b3483d60c5f0246d35e
SURAJ KUMAR <139971831+suraj7547@users.noreply.github.com>
2026-07-28 18:47:56 +0000

    .

--- area_of_rectangle.java
+++ area_of_rectangle.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
 
-public class area_of_rectangle {
+public class area_of_rectangle{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        double area=0;
-        double breadth=0;
-        double length=0;
-
-        System.out.print("Enter Breadth :");
-        breadth=sc.nextDouble();
-
-        System.out.print("Enter length :");
-        length=sc.nextDouble();
-        sc.close();
-
-        area=length*breadth;  
-
-        System.out.println("Area of rectangle is "+area+"cm²");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+        System.out.print("Enter the length of the rectangle: ");
+        int length = scanner.nextInt();
+        System.out.print("Enter breadth of the rectangle: ");
+        int breadth = scanner.nextInt();
+        int area = length * breadth;
+        System.out.println("Area of the reactangle: "+area);
+        scanner.close();
     }
 }
