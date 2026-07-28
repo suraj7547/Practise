65e9d27527a90799534a3132468e3cf723145502
SURAJ KUMAR <139971831+suraj7547@users.noreply.github.com>
2026-07-28 19:06:26 +0000

    quiz second video 32.48

---
+++ quiz.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class quiz {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        int a=sc.nextInt();
+        System.out.print("Enter the value of b: ");
+        int b = sc.nextInt();
+        int total=(a*b)/(a-b);
+        System.out.println("Answer: "+total);
+        sc.close();
+    }
+}
