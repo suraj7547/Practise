394169dbdb276141bf251c6c806d71b763f42566
SURAJ KUMAR <139971831+suraj7547@users.noreply.github.com>
2026-07-29 19:14:13 +0000

    finding even odd with modulo operrator

--- if_else.java
+++ if_else.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
-public class if_else {
+public class if_else{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your age: ");
-        int age = sc.nextInt();
-        if (age <18){
-            System.out.println("You are not eligible to vote");
-        }
-        else 
-        {
-            System.out.println("You are elgible to vote");
+        System.out.println("Enter your number: ");
+        int x = sc.nextInt();
+        if(x%2==0){
+            System.out.println(x+ " is a even number.");
+        }else {
+            System.out.println(x+" is an odd number.");
         }
         sc.close();
     }
 }
-
-
