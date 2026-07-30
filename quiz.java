4e11231611c29b71647bf2bdf7692268d89607c5
SURAJ KUMAR <139971831+suraj7547@users.noreply.github.com>
2026-07-30 09:10:34 +0000

    if else program greeting in different languages

--- quiz.java
+++ quiz.java
@@ -1,14 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class quiz {
+public class quiz{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of a: ");
-        int a=sc.nextInt();
-        System.out.print("Enter the value of b: ");
-        int b = sc.nextInt();
-        int total=(a*b)/(a-b);
-        System.out.println("Answer: "+total);
+        System.out.print("Coose your language ");
+        System.out.print("1.English/2.Hindi/3.Japanese: ");
+        int x = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        if(x==1){
+            System.out.println("Hello "+name);
+        } else if(x==2){
+            System.out.println("Namaste "+name);
+        }else if (x==3){
+            System.out.println("Bonjoyo "+name);
+        }else {
+            System.out.println("Invalid input!");
+        }
         sc.close();
     }
 }
